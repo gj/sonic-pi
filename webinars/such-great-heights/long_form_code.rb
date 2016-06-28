86b11ffa771a5e3686ad315d152065941464c415
ga-be <ga-be@users.noreply.github.com>
2016-06-28 14:28:45 -0400

    Create long_form_code.rb

---
+++ webinars/such-great-heights/long_form_code.rb
@@ -0,0 +1,41 @@
+use_bpm 175
+
+# FIRST MEASURE (first set of 8 notes)
+play :F    # First three notes
+sleep 0.5
+play :C5
+sleep 0.5
+play :F
+sleep 0.5
+
+play :F    # Same three notes, repeated
+sleep 0.5
+play :C5
+sleep 0.5
+play :F
+sleep 0.5
+
+play :F    # Final two notes, lower and then higher
+sleep 0.5
+play :C5
+sleep 0.5
+
+# SECOND MEASURE (next set of 8 notes)
+play :F4   # Same three notes again!
+sleep 0.5
+play :C5
+sleep 0.5
+play :F4
+sleep 0.5
+
+play :F4   # and again!
+sleep 0.5
+play :C5
+sleep 0.5
+play :F4
+sleep 0.5
+
+play :C5   # Final two notes, higher and then lower
+sleep 0.5
+play :F4
+sleep 0.5
