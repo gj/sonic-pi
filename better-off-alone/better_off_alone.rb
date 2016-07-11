5c85dfcbc609cef3df000942d42a57d39d48c4f0
ga-be <ga-be@users.noreply.github.com>
2016-07-11 01:26:37 -0400

    Update better_off_alone.rb

--- better-off-alone/better_off_alone.rb
+++ better-off-alone/better_off_alone.rb
@@ -24,7 +24,7 @@ live_loop :drumrolls do
       sleep 0.25
     end
   end
-  sleep 32
+  sleep 96
 end
 
 live_loop :weird_synth do
@@ -44,14 +44,14 @@ live_loop :weird_synth do
       end
     end
   end
-  sleep 32
+  sleep 96
 end
 
 live_loop :main_synth do
   sleep 4
   use_synth :dsaw
-  use_synth_defaults cutoff: 115, detune: 0.05, sustain: 0.3, release: 0.2, amp: 0.9
-  with_fx :ixi_techno, phase: 256, cutoff_min: 100, res: 0, amp: 0.8 do |ixi|
+  use_synth_defaults cutoff: 115, detune: 0.075, sustain: 0.3, release: 0.2, amp: 0.9
+  with_fx :ixi_techno, phase: 256, cutoff_min: 100, res: 0, amp: 0.7 do |ixi|
     with_fx :echo, mix: 0.07, phase: 1, decay: 8 do
       with_fx :reverb, mix: 0.2, room: 0.3, damp: 0.4 do
         2.times do
@@ -70,6 +70,7 @@ live_loop :main_synth do
           play_pattern_timed [:B4, :B4, :Gs4, :B4, :B4, :As4, :Fs4, :Fs5, :Fs5, :Ds5,
                               :B4, :B4, :Gs4, :B4, :B4, :As4, :Fs4, :E5, :E5, :Ds5], [1, 0.5, 1, 1, 1, 1, 0.5, 0.75, 0.75, 0.5], release: 0.225
         end
+        sleep 64
       end
     end
   end
@@ -91,7 +92,9 @@ live_loop :bass do
     bass
   end
   sleep 32
-  bass
+  3.times do
+    bass
+  end
 end
 
 live_loop :sub_bass do
@@ -102,7 +105,9 @@ live_loop :sub_bass do
       bass
     end
     sleep 32
-    bass
+    3.times do
+      bass
+    end
   end
 end
 
@@ -116,11 +121,13 @@ live_loop :kick do
     end
   end
   sleep 35
-  29.times do
-    sample :bd_haus
-    sleep 1
+  3.times do
+    29.times do
+      sample :bd_haus
+      sleep 1
+    end
+    sleep 3
   end
-  sleep 3
 end
 
 live_loop :hihat do # hi-hat inspiration from Jindřich Mynarz
@@ -136,36 +143,59 @@ live_loop :hihat do # hi-hat inspiration from Jindřich Mynarz
     sleep 0.5
   end
   sleep 36
-  with_fx :rhpf, cutoff: 122, res: 0.75, reps: 56 do
-    play :E1, attack: 0.01, decay: 0.04, release: 0.05, amp: 0.9 if (spread 3, 4, rotate: 2).tick
-    sleep 0.5
+  3.times do
+    with_fx :rhpf, cutoff: 122, res: 0.75, reps: 56 do
+      play :E1, attack: 0.01, decay: 0.04, release: 0.05, amp: 0.9 if (spread 3, 4, rotate: 2).tick
+      sleep 0.5
+    end
+    sleep 4
   end
-  sleep 4
 end
 
 live_loop :hihat2 do
-  beat = (ring 0.1, 0.1, 1, 0.1, 0.1, 0.1, 1, 1, 0.1, 0.1, 1, 0.1, 0.1, 0.1, 1, 0.1)
-  2.times do
-    sleep 36
-    with_fx :reverb, room: 0.2, reps: 112 do
-      sample :drum_cymbal_closed, amp: beat.tick, finish: beat.look
-      sleep 0.25
-    end
+  beat1 = (ring 0, 0, 0.4, 0, 0, 0, 0.4, 0.4, 0, 0, 0.4, 0, 0, 0, 0.4, 0)
+  beat2 = (ring 0.1, 0.1, 0.4, 0.1, 0.1, 0.1, 0.4, 0.4, 0.1, 0.1, 0.4, 0.1, 0.1, 0.1, 0.4, 0.1)
+  sleep 36
+  with_fx :reverb, room: 0.2, reps: 112 do
+    sample :drum_cymbal_closed, amp: 2*beat1.tick, finish: beat1.look
+    sleep 0.25
+  end
+  sleep 36
+  with_fx :reverb, room: 0.2, reps: 112 do
+    sample :drum_cymbal_closed, amp: 2*beat2.tick, finish: beat2.look
+    sleep 0.25
   end
   sleep 4
+  2.times do
+    with_fx :reverb, room: 0.2, reps: 112 do
+      sample :drum_cymbal_closed, amp: 2*beat1.tick, finish: beat1.look
+      sleep 0.25
+    end
+    sleep 4
+  end
 end
 
 live_loop :snare do
-  with_fx :normaliser, reps: 2, level: 0.125 do |rev|
-    sleep 36
-    with_fx :rhpf, reps: 14 do
-      sleep 1
-      sample :sn_dolf, start: 0.1, finish: 0.4
-      sleep 1
+  with_fx :normaliser, level: 0.125 do |rev|
+    2.times do
+      sleep 36
+      with_fx :rhpf, reps: 14 do
+        sleep 1
+        sample :sn_dolf, start: 0.1, finish: 0.4
+        sleep 1
+      end
+      control rev, level: 0.25
     end
-    control rev, level: 0.25
+    sleep 4
+    2.times do
+      with_fx :rhpf, reps: 14 do
+        sleep 1
+        sample :sn_dolf, start: 0.1, finish: 0.4
+        sleep 1
+      end
+      sleep 4
+    end
   end
-  sleep 4
 end
 
 live_loop :filter_synth do
@@ -186,6 +216,7 @@ live_loop :filter_synth do
       end
     end
   end
+  sleep 64
 end
 
 def thick_pad(note, key)
@@ -210,4 +241,25 @@ live_loop :pad do
       thick_pad(:Fs2, :major)
     end
   end
+  sleep 64
+end
+
+def trance_synth(order, detune, phase_offset)
+  trance_notes = [[:Gs4, :E4], [:As4, :Fs4], [:B4, :Gs4], [:Ds5, :Gs4]]
+  use_synth :dsaw
+  use_synth_defaults detune: detune, cutoff: 130, env_curve: 4, release: 0.4, amp: 0.8
+  with_fx :ixi_techno, phase: 64, phase_offset: phase_offset, cutoff_min: 110, cutoff_max: 125, res: 0 do
+    with_fx :reverb, mix: 0.2, room: 0.75, damp: 1 do
+      order.each{|order| 4.times do play_pattern_timed trance_notes[order], 0.5 end }
+    end
+  end
+end
+
+live_loop :trance_synth do
+  sleep 132
+  trance_synth([0], 0.12, 0)
+  trance_synth([1], 0.16, 0.0625)
+  trance_synth([2], 0.20, 0.125)
+  trance_synth([1], 0.24, 0.1875)
+  trance_synth([0, 1, 2, 3, 0, 1, 2, 1, 0, 1, 2, 3], 0.28, 0.25)
 end
