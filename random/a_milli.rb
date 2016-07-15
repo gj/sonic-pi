cca9c0ee59d71aaf8ec98507920b630aa465a8a1
gj <gj@users.noreply.github.com>
2016-07-15 17:57:47 -0400

    Update a_milli.rb

--- random/a_milli.rb
+++ random/a_milli.rb
@@ -2,7 +2,7 @@
 use_bpm 76
 
 live_loop :wayne do
-  sample "/path/to/sample/a_milli_clip.flac"
+  sample "/PATH/TO/SAMPLE/a_milli_clip.flac", amp: 1.5
   sleep 8
 end
 
@@ -16,7 +16,14 @@ drum_hash = {
   :bd_boom            => "X--X--X---X--X--",
   
   # uncomment the below if you want a hi-hat
-  ##| :drum_cymbal_closed => "xXxxXxxXxXxxXXXx"
+  :drum_cymbal_closed => "xXxxXxxXxxXxxXxX",
+  
+  # uncomment the below if you want random sounds
+  ##| :ambi_lunar_land    => "X---------------",
+  :ambi_piano         => "X-----O---------",
+  ##| :ambi_glass_hum     => "X---------------",
+  ##| :ambi_drone     => "X---------------",
+  :ambi_soft_buzz     => "X---------O--O--",
 }
 
 live_loop :drums do
@@ -24,6 +31,7 @@ live_loop :drums do
     drum_hash.each do |key, value|
       sample key if value[n] == "X" # capital 'X' = hard hit
       sample key, amp: 0.5, finish: 0.2 if value[n] == "x" # lowercase 'x' = light hit
+      sample key, pitch: choose([-2, 0, 1, 3]) if value[n] == "O"
     end
     sleep 0.25
   end
