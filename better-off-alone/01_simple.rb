d785df4f9dbca472a20b98508edb7800f042eb55
gabe <gj+code@mail.co.de>
2016-08-24 11:38:42 -0400

    Create 01_simple.rb

---
+++ better-off-alone/01_simple.rb
@@ -0,0 +1,92 @@
+live_loop :filter_synth do
+  use_synth :tb303
+  use_synth_defaults release: 0.2, res: 0.9, cutoff_min: 110, cutoff: 130
+  
+  play :E2
+  sleep 0.5
+  play :E2
+  sleep 0.25
+  play :E2
+  sleep 0.5
+  play :E2
+  sleep 0.25
+  play :E2
+  sleep 0.5
+  
+  play :E2
+  sleep 0.5
+  play :E2
+  sleep 0.25
+  play :E2
+  sleep 0.5
+  play :E2
+  sleep 0.25
+  play :E2
+  sleep 0.5
+  
+  play :Ds2
+  sleep 0.5
+  play :Ds2
+  sleep 0.25
+  play :Ds2
+  sleep 0.5
+  play :Ds2
+  sleep 0.25
+  play :Ds2
+  sleep 0.5
+  
+  play :Ds2
+  sleep 0.5
+  play :Ds2
+  sleep 0.25
+  play :Ds2
+  sleep 0.5
+  play :Ds2
+  sleep 0.25
+  play :Ds2
+  sleep 0.5
+  
+  play :Gs2
+  sleep 0.5
+  play :Gs2
+  sleep 0.25
+  play :Gs2
+  sleep 0.5
+  play :Gs2
+  sleep 0.25
+  play :Gs2
+  sleep 0.5
+  
+  play :Gs2
+  sleep 0.5
+  play :Gs2
+  sleep 0.25
+  play :Gs2
+  sleep 0.5
+  play :Gs2
+  sleep 0.25
+  play :Gs2
+  sleep 0.5
+  
+  play :Fs2
+  sleep 0.5
+  play :Fs2
+  sleep 0.25
+  play :Fs2
+  sleep 0.5
+  play :Fs2
+  sleep 0.25
+  play :Fs2
+  sleep 0.5
+  
+  play :Fs2
+  sleep 0.5
+  play :Fs2
+  sleep 0.25
+  play :Fs2
+  sleep 0.5
+  play :Fs2
+  sleep 0.25
+  play :Fs2
+  sleep 0.5
+end
