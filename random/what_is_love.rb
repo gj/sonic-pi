d5d5b73720c68ab0a7d924eff7766f245b9b4cfd
gabe <gj@mail.co.de>
2016-12-09 19:55:36 -0500

    add some panning

--- random/what_is_love.rb
+++ random/what_is_love.rb
@@ -3,7 +3,7 @@ use_bpm 124
 def chip_synth_saw
   use_synth :dsaw
   use_octave 1
-  use_synth_defaults release: 0.5, cutoff: 130, detune: 0.125, env_curve: 7, amp: 0.25
+  use_synth_defaults release: 0.5, cutoff: 130, detune: 0.125, env_curve: 7, amp: 0.25, pan: -0.1
   
   with_fx :ixi_techno, cutoff_min: 100, phase: 16, res: 0.5 do
     with_fx :reverb do
@@ -20,7 +20,7 @@ end
 def chip_synth_square
   use_synth :square
   use_octave 1
-  use_synth_defaults release: 0.5, cutoff: 120, env_curve: 7, amp: 0.75
+  use_synth_defaults release: 0.5, cutoff: 120, env_curve: 7, amp: 0.75, pan: 0.1
   
   with_fx :ixi_techno, cutoff_min: 100, phase: 16, res: 0.5 do
     with_fx :reverb do
@@ -36,7 +36,7 @@ end
 
 def bass_line
   use_synth :fm
-  use_synth_defaults release: 0.75
+  use_synth_defaults release: 0.75, pan: -0.1
   
   play_pattern_timed [:G2, :G2, :G2, :G2, :F2,
                       :Bb2, :Bb2, :Bb2, :C3, :Eb3,
@@ -45,17 +45,17 @@ def bass_line
 end
 
 def play_song
-  in_thread do
-    loop do
-      chip_synth_saw
-    end
-  end
+  ##| in_thread do
+  ##|   loop do
+  ##|     chip_synth_saw
+  ##|   end
+  ##| end
   
-  in_thread do
-    loop do
-      chip_synth_square
-    end
-  end
+  ##| in_thread do
+  ##|   loop do
+  ##|     chip_synth_square
+  ##|   end
+  ##| end
   
   in_thread do
     loop do
